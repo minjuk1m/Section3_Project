5cf3f239e51027c890f2910f36661e1499a535d5
minjuk1m <86721142+minjuk1m@users.noreply.github.com>
2021-10-11 19:22:53 +0900

    Delete request.py

--- request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/predict_api'
-r = requests.post(url, json = {'Country' : 'South Korea', 'Education' : 'Bachelor’s degree', 'Experience' : 4, 'Age' : '25-34 years old' })
-
-print(r.json())
